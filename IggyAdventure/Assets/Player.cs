5cefbefce62f692d15ebb1ce187e6e35475d0901
TrickyMaker <sikri19@student.sdu.dk>
2019-11-16 16:41:25 +0100

    Add files via upload

---
+++ IggyAdventure/Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] private float jumpHeight = 4f;
+	[SerializeField] private float moveSpeed = 2f;
+	private Rigidbody2D m_Rigidbody2D;
+
+
+	private void Awake()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+		}
+
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+				GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+		}
+
+
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+		}
+	}
+}
