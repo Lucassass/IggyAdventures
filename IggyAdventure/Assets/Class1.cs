8c11b74abf9b64eb201fc94f98b5b233a4db00bd
TrickyMaker <sikri19@student.sdu.dk>
2019-11-16 19:58:28 +0100

    Add files via upload

---
+++ IggyAdventure/Assets/Class1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletSpawn : MonoBehaviour
+{
+
+	// Start is called before the first frame update
+	void Start()
+
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
