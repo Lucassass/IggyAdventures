e0a7a383898a23f600e4d705d1176206c38200da
alkna13 <aleclassenknapp@gmail.com>
2019-11-16 19:24:36 +0100

    Add files via upload
    enemies moving back and forth between specific points

---
+++ EnemyPathing.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    public Transform waypoint1;
+    public Transform waypoint2;
+    public float speed;
+    int target = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform targetwaypoint;
+        if (target == 1)
+        {
+            targetwaypoint = waypoint1;
+            //Debug.Log("heading toward 1");
+        }
+        else
+        {
+            targetwaypoint = waypoint2;
+           // Debug.Log("heading toward 2");
+        }
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, targetwaypoint.position, step);
+        if (System.Math.Abs(transform.position.x - targetwaypoint.position.x) < 1)
+        {
+
+            Debug.Log("checkpoint reached");
+
+            if (target == 1) { target++; }
+            else { target--; }
+
+        }
+
+    }
+}
+
